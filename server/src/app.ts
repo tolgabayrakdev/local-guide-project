2e70d883a20427f825788ab04272c594f1fa9fb8
tolgabayrakdev <bayraktolga28@gmail.com>
2023-10-14 20:56:29 +0300

    prettier and depencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,17 @@
-import express, {Express} from "express";
+import express, { Express } from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
 
 const app: Express = express();
 
+
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true}));
+app.use(morgan("combined"));
 
-
-app.listen(1234,() => {
+app.listen(1234, () => {
     console.log(`[Server]: Node server running on port: 1234`);
-
 })
 
